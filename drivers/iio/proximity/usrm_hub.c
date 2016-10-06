e3e9529addd76811f50cee4cfcc7bbe5fcd00d17
ray <ray@eleks.com>
2016-10-06 19:42:45 +0300

    USRM-HUB driver scan channels added

--- drivers/iio/proximity/usrm_hub.c
+++ drivers/iio/proximity/usrm_hub.c
@@ -28,8 +28,8 @@ static int usrm_read_raw(struct iio_dev *indio_dev,
 		if (ret < 0)
 			return ret;
 		if (chan->channel >= 0 && chan->channel < CHAN_COUNT) {
-			*val = rxData[chan->channel * 2];				/* LSB */
-			*val = (u16)rxData[chan->channel * 2 + 1] << 8;	/* MSB */
+			*val = rxData[chan->channel * 2];					/* LSB */
+			*val += (u16)rxData[chan->channel * 2 + 1] << 8;	/* MSB */
 		}
 		else {
 			*val = 0;
@@ -46,25 +46,34 @@ static int usrm_read_raw(struct iio_dev *indio_dev,
 	return -EINVAL;
 }
 
-#define USRM_CHAN(_channel)			\
+#define USRM_CHAN(_channel, _si)	\
 		{							\
 			.type = IIO_DISTANCE,	\
 			.indexed = 1,			\
 			.channel = _channel,	\
-			.info_mask_separate = BIT(IIO_CHAN_INFO_PROCESSED),		\
-			.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_SAMP_FREQ)	\
+			.info_mask_separate = BIT(IIO_CHAN_INFO_PROCESSED),			\
+			.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_SAMP_FREQ),	\
+			.scan_index = _si,			\
+			.scan_type = {					\
+					.sign = 'u',			\
+					.realbits = 16,			\
+					.storagebits = 16,		\
+					.shift = 0,				\
+					.endianness = IIO_LE,	\
+			},								\
 		}
 
 
 static const struct iio_chan_spec usrm_channels[] = {
-		USRM_CHAN(0),
-		USRM_CHAN(1),
-		USRM_CHAN(2),
-		USRM_CHAN(3),
-		USRM_CHAN(4),
-		USRM_CHAN(5),
-		USRM_CHAN(6),
-		USRM_CHAN(7)
+		IIO_CHAN_SOFT_TIMESTAMP(0),
+		USRM_CHAN(0, 1),
+		USRM_CHAN(1, 2),
+		USRM_CHAN(2, 3),
+		USRM_CHAN(3, 4),
+		USRM_CHAN(4, 5),
+		USRM_CHAN(5, 6),
+		USRM_CHAN(6, 7),
+		USRM_CHAN(7, 8)
 };
 
 static const struct iio_info usrm_info = {
@@ -77,7 +86,6 @@ static int usrm_probe(struct i2c_client *client,
 {
 	struct iio_dev *indio_dev;
 	struct i2c_client **data;
-	int ret;
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_I2C_BLOCK))
 		return -ENODEV;
